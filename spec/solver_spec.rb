b46212d8471928b51b4e14fd90bdc1261bc3b276
Juan Diego Guerra <jdxelric@gmail.com>
2023-10-12 06:03:38 -0500

    Setup main files for the project

---
+++ spec/solver_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require_relative '../solver'
+
+describe Solver do
+  before(:each) do
+    @solver = Solver.new
+  end
+
+  # tests for factorial method
+
+  # test for reverse method
+
+  # test for fizzbuzz method
