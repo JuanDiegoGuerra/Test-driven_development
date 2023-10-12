a1faa7481efde9566a492633d6051ae431796eab
hafiz1379 <hafizrasa1379@gmail.com>
2023-10-12 15:51:50 +0430

    RSpec tests for Solver's factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,6 +7,17 @@ describe Solver do
   end
 
   # tests for factorial method
+  it 'get factorial of number 6' do
+    result = @solver.factorial(6)
+    expect(result).to eq(720)
+  end
+  it 'get factorial of number 0' do
+    result = @solver.factorial(0)
+    expect(result).to eq(1)
+  end
+  it 'raise an exception for negative number' do
+    expect { @solver.factorial(-1) }.to raise_error(RuntimeError, 'Argument cannot be negative')
+  end
 
   # test for reverse method
 
