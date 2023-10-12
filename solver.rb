03e1d882f8857a24826a5b80405d9d3e000cd5a6
Juan Diego Guerra <jdxelric@gmail.com>
2023-10-12 06:31:05 -0500

    Create logic for main methods

--- solver.rb
+++ solver.rb
@@ -1,3 +1,31 @@
 class Solver
-
-end
+    def factorial(num)
+      if num.zero?
+        return 1
+      elsif num == 1
+        return num
+      elsif num.negative?
+        raise 'Argument cannot be negative'
+      end
+  
+      num * factorial(num - 1)
+    end
+  
+    def reverse(word)
+      return 'Argument cannot be an integer' unless word.is_a?(String)
+  
+      word.reverse
+    end
+  
+    def fizzbuzz(num)
+      if (num % 3).zero? && (num % 5).zero?
+        'fizzbuzz'
+      elsif (num % 3).zero?
+        'fizz'
+      elsif (num % 5).zero?
+        'buzz'
+      else
+        num.to_s
+      end
+    end
+  end
